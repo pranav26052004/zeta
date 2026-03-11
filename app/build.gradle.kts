bf3c5d26642a40376fe8034c4b6494191b0d6aa0
Pranav <domdalapranav@gmail.com>
2026-03-11 13:30:48 +0530

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 }
 
 dependencies {
-    implementation("com.google.firebase:firebase-database")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
