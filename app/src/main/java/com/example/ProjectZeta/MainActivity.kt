da875dbc8ce6b3eaca6e8057290b4632c1882c03
Arnika <arnikarao@gmail.com>
2026-03-12 10:00:15 +0530

    Find All Notes Screen Added

--- app/src/main/java/com/example/ProjectZeta/MainActivity.kt
+++ app/src/main/java/com/example/ProjectZeta/MainActivity.kt
@@ -104,10 +104,11 @@ class MainActivity : ComponentActivity() {
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
-                    parkingScreen()
+//                    parkingScreen()
 //                    HomeScreen()
 //                    SignUpScreen()
-                    LostAndFoundScreen()
+//                    LostAndFoundScreen()
+                    FindAllNotes()
                 }
             }
         }
@@ -692,7 +693,6 @@ fun LostAndFoundScreen() {
     }
 }
 
-
 @Composable
 fun Login(){
     val username by rememberSaveable { mutableStateOf("") }
@@ -830,7 +830,6 @@ fun AboutPage() {
             ) {
                 Text("Save changes")
             }
-//            Spacer(modifier = Modifier.height(30.dp))
 
             Button(
                 onClick = {},
@@ -977,4 +976,80 @@ fun UploadNotes(){
     }
 }
 
+@Composable
+fun FindAllNotes(){
+    var search by remember { mutableStateOf("") }
+    var selectedtabindex by remember { mutableStateOf(0) }
+    var footerindex by remember { mutableStateOf(0) }
+    val footerr: List<Int> = listOf(
+        R.drawable.baseline_home_24,
+        R.drawable.outline_feature_search_24,
+        R.drawable.baseline_local_parking_24,
+        R.drawable.outline_person_24
+    )
+    Column(Modifier.fillMaxSize()) {
+        OutlinedTextField(
+            value = search,
+            onValueChange = { search = it },
+            Modifier.fillMaxWidth().padding(top = 30.dp),
+            label = { Text("Search") },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search icon"
+                )
+            }
+        )
 
+        Card(
+            modifier = Modifier
+                .weight(2f)
+                .fillMaxWidth().padding(top = 30.dp)
+                .padding(horizontal = 14.dp, vertical = 5.dp)
+        ){
+            LazyColumn (
+                modifier = Modifier.fillMaxSize()
+            ){
+                stickyHeader { Text("Find All the Notes Here!",
+                    Modifier.padding(start = 70.dp), fontWeight = FontWeight.Bold, fontSize = 20.sp) }
+
+            }
+        }
+
+        TabRow(
+            selectedTabIndex = footerindex,
+            divider = { HorizontalDivider() },
+            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            modifier = Modifier
+                .padding(top = 130.dp)
+                .fillMaxWidth().size(100.dp)
+                .drawBehind {
+                    val x = size.width / 2f
+                    drawLine(
+                        color = Color.Black,
+                        start = Offset(x, 0f),
+                        end = Offset(x, size.height),
+                        strokeWidth = 2.dp.toPx()
+                    )
+                }
+        ) {
+            footerr.forEachIndexed { index, icon ->
+                Tab(
+                    selected = footerindex == index,
+                    onClick = {
+                        footerindex = index
+                        if (index == 1) {
+                            selectedtabindex = 0
+                        }
+                    },
+                    icon = {
+                        Image(
+                            painterResource(icon),
+                            contentDescription = null
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
